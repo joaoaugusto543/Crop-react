5d5b5f8082f3c897684a9fae81e00511260d7c31
Pedro Correia Lopes <pedrohenriquecorreialopes@gmail.com>
2023-11-30 15:44:16 -0300

    Ajustes routes

--- crop/src/App.js
+++ crop/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, BrowserRouter} from 'react-router-dom'
 import Home from './pages/Home/Home'
+
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>   
   )
 }
 
